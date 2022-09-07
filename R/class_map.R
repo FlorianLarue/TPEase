5eb06af4859990f67fedafc499eec85ec915acd4
Florian Larue <contact.florian.larue@gmail.com>
2022-09-07 07:56:58 +0200

    Added map class

---
+++ R/class_map.R
@@ -0,0 +1,27 @@
+#'#' R6 Class Representing a TPE map
+#'
+#' @description
+#' TODO
+#'
+#' @details
+#' TODO
+#' @import R6
+#' @export
+TPEmap <- R6::R6Class("TPEmap",
+  public = list(
+    #' @field name A character string identifier of the map
+    name = NULL,
+    #' @field test Debug
+    test = NA,
+
+    #' @description Create a new TPE analysis object
+    #' @param name A character string identifier of the TPE analysis
+    #' @return A new `TPEmap` object.
+    initialize = function(name="map1") {
+      self$name <- as.character(name)
+    }
+  ),
+
+  private = list(
+  )
+)
